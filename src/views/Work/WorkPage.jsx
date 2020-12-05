4b14fdfa7c8ad702d63f30a87f3fc90314301790
duongductrong <duongductrong06@gmail.com>
2020-12-05 11:02:44 +0700

    add animation

--- src/views/Work/WorkPage.jsx
+++ src/views/Work/WorkPage.jsx
@@ -1,10 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { TimelineLite } from "gsap";
 import "./WorkPage.scss";
 
 import Typography from "../../components/Dumb/Typography/Typography";
 import Separate from "../../components/Dumb/Sepeate/Separate";
 
 function WorkPage() {
+  const timeline = new TimelineLite();
+
+  const [data] = useState([
+    {
+      key: 1,
+      title: "XskillStore - Ecommere (Personal)",
+      href: "https://reactjs-xskillstore.firebaseapp.com/",
+      when: "2019",
+      description:
+        "Là trang web đầu tiên trong quá trình tự học frontend của mình. Xây dựng đầy đầy đủ các tính năng cơ bản của 1 trang ecommere. Trang các công nghệ React/ExpressJS/Mongodb",
+      refTitle: "",
+      refDescription: "",
+    },
+
+    {
+      key: 2,
+      title: "Leon blog (Personal)",
+      href: "http://leon-sand.firebaseapp.com/",
+      when: "2019",
+      description:
+        "Là trang blog cá nhân được mình xây dựng trong quá trình tự học. Sử dụng công nghệ React/ExpressJS/Mongodb",
+      refTitle: "",
+      refDescription: "",
+    },
+
+    {
+      key: 3,
+      title: "Xây dựng Frontend CMS crawling data Ecommere (Khả kim)",
+      href: "#",
+      when: "2020",
+      description: `Ở đây mình cùng với một đồng nghiệp khác sử dụng framework VueJS của
+        JavaScript để xây dựng giao diện, xử lý dữ liệu từ API và các nghiệp
+        vụ liên quan để xây dựng CMS Crawling Data Ecommere - Canawan (Khả
+        kim)`,
+      refTitle: "",
+      refDescription: "",
+    },
+
+    {
+      key: 4,
+      title: "Hạ tầng đô thị (TQDesign)",
+      href: "https://hatangdothi.tqdesign.vn/",
+      when: "2020",
+      description: `Mình đảm nhận dự án cùng với đồng nghiệp xây dựng frontend cho trang
+        Hạ tần đô thị (TQDesign)`,
+      refTitle: "",
+      refDescription: "",
+    },
+
+    {
+      key: 5,
+      title: "Trang phả đồ thế hệ (Private)",
+      href: "https://nguyenquang-thaibinh.com/",
+      when: "2020",
+      description: `Đảm nhận trách nhiệm xây dựng cả frontend và backend cho dự án
+      website về phả đồ thế hệ. Sử dụng công nghệ Laravel/Mysql`,
+      refTitle: "",
+      refDescription: "",
+    },
+  ]);
+
+  useEffect(() => {
+    document.querySelectorAll(".col-12").forEach((item, i) => {
+      timeline.fromTo(
+        item,
+        {
+          yPercent: 25,
+          opacity: 0,
+          duration: 0.3,
+        },
+        {
+          yPercent: 0,
+          opacity: 1,
+          duration: 0.3,
+        }
+      );
+    });
+  }, []);
+
   return (
     <div className="works">
       <Typography variant="h3">Works</Typography>
@@ -19,101 +99,21 @@ function WorkPage() {
 
       <Separate variant="dashed" />
 
-      <div className="row">
-        <div className="col-12 col-768-4">
-          <Typography variant="span" fontWeight="bold">
-            <a href="https://reactjs-xskillstore.firebaseapp.com/">
-              XskillStore - Ecommere (Personal)
-            </a>
-          </Typography>
-          <Typography variant="span" fontWeight="lighter" block>
-            2019
-          </Typography>
+      {data.map((item) => (
+        <div key={item.key} className="row">
+          <div className="col-12 col-768-4">
+            <Typography variant="span" fontWeight="bold">
+              <a href={item.href}>{item.title}</a>
+            </Typography>
+            <Typography variant="span" fontWeight="lighter" block>
+              {item.when}
+            </Typography>
+          </div>
+          <div className="col-12 col-768-8">
+            <Typography variant="p">{item.description}</Typography>
+          </div>
         </div>
-        <div className="col-12 col-768-8">
-          <Typography variant="p">
-            Là trang web đầu tiên trong quá trình tự học frontend của mình. Xây
-            dựng đầy đầy đủ các tính năng cơ bản của 1 trang ecommere. Trang sử
-            các công nghệ React/ExpressJS/Mongodb
-          </Typography>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-12 col-768-4">
-          <Typography variant="span" fontWeight="bold">
-            <a href="http://leon-sand.firebaseapp.com/">Leon blog (Personal)</a>
-          </Typography>
-          <Typography variant="span" fontWeight="lighter" block>
-            2019
-          </Typography>
-        </div>
-        <div className="col-12 col-768-8">
-          <Typography variant="p">
-            Là trang blog cá nhân được mình xây dựng trong quá trình tự học. Sử
-            dụng công nghệ React/ExpressJS/Mongodb
-          </Typography>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-12 col-768-4">
-          <Typography variant="span" fontWeight="bold">
-            <a href="#">
-              Xây dựng giao diện CMS crawling data Ecommere (Khả kim)
-            </a>
-          </Typography>
-          <Typography variant="span" fontWeight="lighter" block>
-            2020
-          </Typography>
-        </div>
-        <div className="col-12 col-768-8">
-          <Typography variant="p">
-            Ở đây mình cùng với một đồng nghiệp khác sử dụng framework VueJS của
-            JavaScript để xây dựng giao diện, xử lý dữ liệu từ API và các nghiệp
-            vụ liên quan để xây dựng CMS Crawling Data Ecommere - Canawan (Khả
-            kim)
-          </Typography>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-12 col-768-4">
-          <Typography variant="span" fontWeight="bold">
-            <a href="https://hatangdothi.tqdesign.vn/">
-              Hạ tầng đô thị (TQDesign)
-            </a>
-          </Typography>
-          <Typography variant="span" fontWeight="lighter" block>
-            2020
-          </Typography>
-        </div>
-        <div className="col-12 col-768-8">
-          <Typography variant="p">
-            Mình đảm nhận dự án cùng với đồng nghiệp xây dựng frontend cho trang
-            Hạ tần đô thị (TQDesign)
-          </Typography>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-12 col-768-4">
-          <Typography variant="span" fontWeight="bold">
-            <a href="https://nguyenquang-thaibinh.com/">
-              Trang Phả đồ thế hệ (Private)
-            </a>
-          </Typography>
-          <Typography variant="span" fontWeight="lighter" block>
-            2020
-          </Typography>
-        </div>
-        <div className="col-12 col-768-8">
-          <Typography variant="p">
-            Đảm nhận trách nhiệm xây dựng cả frontend và backend cho dự án
-            website về phả đồ thế hệ. Sử dụng công nghệ Laravel/Mysql
-          </Typography>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
